8d1732b497afbdaa1688ce89295e07fcfcbaea87
AGsolFCD <fernando.cipriano@agsolution.es>
2022-10-17 09:19:43 +0200

    Comentario final sesión 2

---
+++ NZWalks.API/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; } //If a table doesn't exist, please, create a table
+
+        public DbSet<Walk> Walks { get; set; } //If a table doesn't exist, please, create a table
+
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; } //If a table doesn't exist, please, create a table
+    }
+}
---
+++ NZWalks.API/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,20 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Length { get; set; }
+
+        public Guid RegionID { get; set; }
+
+        public Guid WalkDifficultyID { get; set; }
+
+        //Navigation Properties
+        public Region Region { get; set; }
+
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
